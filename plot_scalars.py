dfbb40b761cb37205044698b80abbb2b9424f1e9
Benjamin Brown <bpbrown@gmail.com>
2025-03-07 12:32:38 +0100

    Improvements to plot_scalars.

--- plot_scalars.py
+++ plot_scalars.py
@@ -47,47 +47,46 @@ if args['--times']:
 else:
     subrange = False
 
+fig, ax = plt.subplots()
+ax.plot(t, data['Re'], label='Re')
+ax_r = ax.twinx()
+ax_r.plot(t, data['Ma'], label='Ma', linestyle='dotted', color='tab:red')
+handles_l, labels_l = ax.get_legend_handles_labels()
+handles_r, labels_r = ax_r.get_legend_handles_labels()
+handles = handles_l + handles_r
+labels = labels_l + labels_r
+ax.legend(handles,labels)
+fig.savefig('{:s}/fluid_properties.png'.format(str(output_path)), dpi=300)
+
 energy_keys = ['KE','IE']#,'PE']
-
-fig_E, ax_E = plt.subplots(nrows=2)
+fig_E, ax_E = plt.subplots()
 for key in energy_keys:
-    ax_E[0].plot(t, data[key], label=key)
-ax_E[1].plot(t, data['KE'], label='KE')
-ax2 = ax_E[1].twinx()
-ax2.plot(t, data['Re'], label='Re', linestyle='dotted')
-ax2.legend(loc='upper left')
+    ax_E.plot(t, data[key], label=key)
+if subrange:
+    ax.set_xlim(t_min,t_max)
+ax.set_xlabel('time')
+ax.set_ylabel('energy density')
+ax.legend(loc='lower left')
+fig_E.savefig('{:s}/energies.png'.format(str(output_path)), dpi=300)
+ax.set_yscale('log')
+fig_E.savefig('{:s}/log_energies.png'.format(str(output_path)), dpi=300)
 
-for ax in ax_E:
-    if subrange:
-        ax.set_xlim(t_min,t_max)
-    ax.set_xlabel('time')
-    ax.set_ylabel('energy density')
-    ax.legend(loc='lower left')
-fig_E.savefig('{:s}/energies.pdf'.format(str(output_path)))
-for ax in ax_E:
-    ax.set_yscale('log')
-fig_E.savefig('{:s}/log_energies.pdf'.format(str(output_path)))
-
-fig_E, ax_E = plt.subplots(nrows=2)
+fig_E, ax_E = plt.subplots()
 for key in energy_keys:
-    ax_E[0].plot(t, data[key]-data[key][0], label=key+"'")
-ax_E[1].plot(t, data['KE'], label='KE')
-
-for ax in ax_E:
-    if subrange:
-        ax.set_xlim(t_min,t_max)
-    ax.set_xlabel('time')
-    ax.set_ylabel('energy density')
-    ax.legend(loc='lower left')
-fig_E.savefig('{:s}/energies_fluctuating.pdf'.format(str(output_path)))
+    ax_E.plot(t, data[key]-data[key][0], label=key+"'")
+if subrange:
+    ax.set_xlim(t_min,t_max)
+ax.set_xlabel('time')
+ax.set_ylabel('energy density')
+ax.legend(loc='lower left')
+fig_E.savefig('{:s}/energies_fluctuating.png'.format(str(output_path)), dpi=300)
 
 
 fig_tau, ax_tau = plt.subplots(nrows=2)
 for i in [0,1]:
-    ax_tau[i].plot(t, data['τ_u1'], label=r'$\tau_{u1}$')
-    ax_tau[i].plot(t, data['τ_u2'], label=r'$\tau_{u2}$')
-    ax_tau[i].plot(t, data['τ_s1'], label=r'$\tau_{s1}$')
-    ax_tau[i].plot(t, data['τ_s2'], label=r'$\tau_{s2}$')
+    ax_tau[i].plot(t, data['τ_u'], label=r'$\tau_{u}$')
+    ax_tau[i].plot(t, data['τ_s'], label=r'$\tau_{s}$')
+    ax_tau[i].plot(t, data['τ_ρ'], label=r'$\tau_{\rho}$')
 
 for ax in ax_tau:
     if subrange:
@@ -96,9 +95,9 @@ for ax in ax_tau:
     ax.set_ylabel(r'$L_\inf(\tau)$')
     ax.legend(loc='lower left')
 ax_tau[1].set_yscale('log')
-fig_tau.savefig('{:s}/tau_error.pdf'.format(str(output_path)))
+fig_tau.savefig('{:s}/tau_error.png'.format(str(output_path)), dpi=300)
 
-benchmark_set = ['KE', 'IE', 'Re']
+benchmark_set = ['KE', 'IE', 'Re', 'Ma']
 i_ten = int(0.9*data[benchmark_set[0]].shape[0])
 for benchmark in benchmark_set:
     print("{:s} = {:14.12g} +- {:4.2g} (averaged from {:g}-{:g})".format(benchmark, np.mean(data[benchmark][i_ten:]), np.std(data[benchmark][i_ten:]), t[i_ten], t[-1]))
