248aa697db616c7fdd2e30cf75e0e735b1152dcb
Benjamin Brown <bpbrown@gmail.com>
2025-03-07 12:32:08 +0100

    Makes plot_slices compatible with 2.5d.  Adjusts various aspects of plotting.

--- plot_slices.py
+++ plot_slices.py
@@ -6,7 +6,7 @@ Usage:
 
 Options:
     --output=<dir>     Output directory; defaults to 'frames' subdir within the case dir
-    --tasks=<tasks>    Tasks to plot [default: s,vorticity]
+    --tasks=<tasks>    Tasks to plot [default: s,s_fluc,vorticity]
 """
 
 import h5py
@@ -38,13 +38,13 @@ def main(filename, start, count, tasks, output):
             title = task
             savename_func = lambda write: '{:s}_{:06d}.png'.format(title, write)
             data = f['tasks'][task]
-            x = data.dims[1][0][:]
+            x = data.dims[2][0][:]
             z = data.dims[3][0][:]
             Lz = np.max(z)-np.min(z)
             Lx = np.max(x)-np.min(x)
             figsize = (6.4, 1.2*Lz/Lx*6.4)
             for k in range(len(t)):
-                data_slice = (k,slice(None),0,slice(None))
+                data_slice = (k,0,slice(None),slice(None))
                 time = t[k]
                 fig, ax = plt.subplots(1, figsize=figsize)
                 ax.set_aspect(1)
@@ -53,27 +53,30 @@ def main(filename, start, count, tasks, output):
                 if title == 'vorticity':
                     cmap = 'PiYG'
                     center_zero = True
+                elif title == 's_fluc':
+                    cmap = 'RdYlBu_r'
+                    center_zero = True
                 else:
                     cmap = None
                     center_zero = False
 
                 if center_zero and not np.allclose([pmin, pmax], [0,0]):
                     cNorm = matplotlib.colors.TwoSlopeNorm(vmin=pmin, vcenter=0, vmax=pmax)
-                    logger.info("centering zero: {} -- 0 -- {}".format(pmin, pmax))
+                    logger.debug("centering zero: {:.2e} -- 0 -- {:.2e}".format(pmin, pmax))
                 else:
                     cNorm = matplotlib.colors.Normalize(vmin=pmin, vmax=pmax)
                 pcm = ax.pcolormesh(x, z, data[data_slice].T, shading='nearest',cmap=cmap, norm=cNorm)
-                ax_cb = fig.add_axes([0.91, 0.4, 0.02, 1-0.4*2])
+                ax_cb = fig.add_axes([0.89, 0.4, 0.02, 1-0.4*2])
                 cb = fig.colorbar(pcm, cax=ax_cb)
                 cb.formatter.set_scientific(True)
                 cb.formatter.set_powerlimits((0,4))
                 cb.ax.yaxis.set_offset_position('left')
                 cb.update_ticks()
-                fig.subplots_adjust(left=0.1,right=0.9,top=0.95)
+                fig.subplots_adjust(left=0.075,right=0.875,top=0.95)
                 if title is not None:
-                    ax_cb.text(0.5, 1.75, title, horizontalalignment='center', verticalalignment='center', transform=ax_cb.transAxes)
+                    ax_cb.text(0.5, 1.75, title, horizontalalignment='left', verticalalignment='center', transform=ax_cb.transAxes)
                 if time is not None:
-                    ax_cb.text(0.25, -0.5, "t = {:.0f}".format(time), horizontalalignment='left', verticalalignment='center', transform=ax_cb.transAxes)
+                    ax.text(0.95, -0.1, "t = {:.0f}".format(time), horizontalalignment='left', verticalalignment='center',transform=ax.transAxes)
                 savename = savename_func(f['scales/write_number'][k])
                 savepath = output.joinpath(savename)
                 fig.savefig(str(savepath), dpi=dpi)
