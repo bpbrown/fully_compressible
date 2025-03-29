6b913913f608af588bd6825558f143810277e72a
Benjamin Brown <bpbrown@gmail.com>
2025-03-29 12:26:37 -0700

    Updates plot_fluxes, goes to png outputs.

--- plot_fluxes.py
+++ plot_fluxes.py
@@ -7,6 +7,8 @@ Usage:
 Options:
     --MESA               Overlay MESA L_MLT
     --output=<output>    Output directory; if blank a guess based on likely case name will be made
+
+    --fraction=<frac>    Fraction of time to average over, from end of simulation [default: 0.1]
 """
 import numpy as np
 
@@ -36,8 +38,6 @@ if args['--output'] is not None:
 else:
     data_dir = case +'/'
     output_path = pathlib.Path(data_dir).absolute()
-
-fields = ['s(z)', 's0(z)'] #'F_h(z)', 'F_κ(z)', 'F_KE(z)', 'F_PE(z)', 'Q_source(z)']
 
 def accumulate_files(filename,start,count,file_list):
     if start==0:
@@ -69,6 +69,16 @@ for file in file_list:
         times = np.append(times, f['scales/sim_time'][:])
     f.close()
 
+n_times = times.shape[0]
+if args['--fraction']:
+    i_cut = int((1-float(args['--fraction']))*n_times)
+    times = times[i_cut:]
+    for task in data:
+        data[task] = data[task][i_cut:]
+
+logger.info("averaged from t={:.3g}--{:.3g}".format(min(times),max(times)))
+
+
 print(file_list)
 for task in data:
     print(task, data[task].shape)
@@ -85,43 +95,80 @@ fig_s.subplots_adjust(top=0.9, right=0.95, bottom=0.2, left=0.15)
 #     ax_s.plot(z, si, alpha=0.3)
 ax_s.plot(z, s_avg, linewidth=2, color='black')
 ax_s.plot(z, s0_avg, linewidth=2, color='black')
-fig_s.savefig('{:s}/thermal_profile.pdf'.format(str(output_path)))
+fig_s.savefig('{:s}/thermal_profile.png'.format(str(output_path)), dpi=300)
+
+Ma_avg = time_avg(data['Ma(z)'])
+Re_avg = time_avg(data['Re(z)'])
+
+fig, ax = plt.subplots(figsize=(6,6/1.62))
+fig.subplots_adjust(top=0.9, right=0.95, bottom=0.2, left=0.15)
+ax.plot(z, Re_avg, linewidth=2, label='Re')
+ax_r = ax.twinx()
+ax_r.plot(z, Ma_avg, linewidth=2, label='Ma', linestyle='dotted', color='tab:red')
+handles_l, labels_l = ax.get_legend_handles_labels()
+handles_r, labels_r = ax_r.get_legend_handles_labels()
+handles = handles_l + handles_r
+labels = labels_l + labels_r
+ax.legend(handles,labels)
+fig.savefig('{:s}/fluid_properties_with_depth.png'.format(str(output_path)), dpi=300)
+
+
 
 F_h = time_avg(data['F_h(z)'])
 F_κ = time_avg(data['F_κ(z)'])
 F_KE = time_avg(data['F_KE(z)'])
-F_PE = time_avg(data['F_PE(z)'])
-Q_source = time_avg(data['Q_source(z)'])
-#F_μ_avg = time_avg(data['<Fμr>'])
+#F_PE = time_avg(data['F_PE(z)'])
+# Q_source = time_avg(data['Q_source(z)'])
+Q_source = 0
+# #F_μ_avg = time_avg(data['<Fμr>'])
+#
+# fig_Q, ax_Q = plt.subplots(figsize=(4.5,4/1.5))
+# fig_Q.subplots_adjust(top=0.9, right=0.8, bottom=0.2, left=0.2)
+# ax_Q.plot(z, Q_source)
+# ax_Q.set_ylabel(r'$\mathcal{S}(z)$')
+# ax_Q.set_xlabel(r'$z$')
+# fig_Q.savefig('{:s}/source_function.pdf'.format(str(output_path)))
 
-fig_Q, ax_Q = plt.subplots(figsize=(4.5,4/1.5))
-fig_Q.subplots_adjust(top=0.9, right=0.8, bottom=0.2, left=0.2)
-ax_Q.plot(z, Q_source)
-ax_Q.set_ylabel(r'$\mathcal{S}(z)$')
-ax_Q.set_xlabel(r'$z$')
-fig_Q.savefig('{:s}/source_function.pdf'.format(str(output_path)))
-
-L_S = sci.cumtrapz(Q_source, x=z, initial=0)
-norm = 1/L_S[-1]
+# L_S = sci.cumtrapz(Q_source, x=z, initial=0)
+# norm = 1/L_S[-1]
+L_S = np.zeros_like(F_h)
+norm = 1
 
 L_h = F_h*norm
 L_κ = F_κ*norm
 L_KE = F_KE*norm
-L_PE = F_PE*norm
+L_PE = np.zeros_like(L_h) #F_PE*norm
 #L_μ = 4*np.pi*r**2*theta_avg(F_μ_avg)*norm
 L_S = L_S*norm
 
-fig_hr, ax_hr = plt.subplots(figsize=(4.5,4/1.5))
-fig_hr.subplots_adjust(top=0.9, right=0.95, bottom=0.2, left=0.15)
-ax_hr.plot(z, L_h + L_KE + L_PE + L_κ, color='black', label=r'$L_\mathrm{tot}$', linewidth=3)
-ax_hr.plot(z, L_h, label=r'$L_\mathrm{h}$')
-ax_hr.plot(z, L_KE, label=r'$L_\mathrm{KE}$')
-ax_hr.plot(z, L_PE, label=r'$L_\mathrm{PE}$')
-ax_hr.plot(z, L_κ, label=r'$L_\kappa$')
-ax_hr.plot(z, L_S, label=r'$L_\mathcal{S}$')
-ax_hr.axhline(y=0, linestyle='dashed', color='darkgrey', zorder=0)
-#ax_hr.plot(r, L_μ, label=r'$L_\mu$')
-ax_hr.legend()
-ax_hr.set_ylabel(r'$L/L_{\scrS(z=Lz)}$')
-ax_hr.set_xlabel(r'$z$')
-fig_hr.savefig('{:s}/flux_balance.pdf'.format(str(output_path)))
+fig, ax = plt.subplots(figsize=(4.5,4/1.5))
+fig.subplots_adjust(top=0.9, right=0.95, bottom=0.2, left=0.15)
+ax.plot(z, L_h + L_KE + L_PE + L_κ, color='black', label=r'$L_\mathrm{tot}$', linewidth=3)
+ax.plot(z, L_h, label=r'$L_\mathrm{h}$')
+ax.plot(z, L_KE, label=r'$L_\mathrm{KE}$')
+#ax.plot(z, L_PE, label=r'$L_\mathrm{PE}$')
+ax.plot(z, L_κ, label=r'$L_\kappa$')
+#ax.plot(z, L_S, label=r'$L_\mathcal{S}$')
+#ax.axhline(y=0, linestyle='dashed', color='darkgrey', zorder=0)
+#ax.plot(r, L_μ, label=r'$L_\mu$')
+ax.legend()
+ax.set_ylabel(r'$L/L_{\mathcal{S}(z=Lz)}$')
+ax.set_xlabel(r'$z$')
+fig.savefig('{:s}/flux_balance.png'.format(str(output_path)), dpi=300)
+
+L_κ_fluc = L_κ - L_κ[-1]
+
+fig, ax = plt.subplots(figsize=(4.5,4/1.5))
+fig.subplots_adjust(top=0.9, right=0.95, bottom=0.2, left=0.15)
+ax.plot(z, L_h + L_KE + L_PE + L_κ_fluc, color='black', label=r'$L_\mathrm{tot}$', linewidth=3)
+ax.plot(z, L_h, label=r'$L_\mathrm{h}$')
+ax.plot(z, L_KE, label=r'$L_\mathrm{KE}$')
+#ax.plot(z, L_PE, label=r'$L_\mathrm{PE}$')
+ax.plot(z, L_κ_fluc, label=r'$L_\kappa$')
+#ax.plot(z, L_S, label=r'$L_\mathcal{S}$')
+#ax.axhline(y=0, linestyle='dashed', color='darkgrey', zorder=0)
+#ax.plot(r, L_μ, label=r'$L_\mu$')
+ax.legend()
+ax.set_ylabel(r'$L/L_{\mathcal{S}(z=Lz)}$')
+ax.set_xlabel(r'$z$')
+fig.savefig('{:s}/flux_balance.png'.format(str(output_path)), dpi=300)
